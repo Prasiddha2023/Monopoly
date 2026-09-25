2420419280c6f1157a431535429f024dc8fe8ee0
Prasiddha Paudyal <ppaudya1@students.towson.edu>
2026-09-25 19:41:33 -0400

    Task 12- I added @override in CardsTest.java and PropertyCell.java, removed the "Integer" constructor in GameBoard.java, and declared chanceCard on a new line in CardsTest.java

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
@@ -45,7 +45,7 @@ public class GameBoard {
 	public void addCell(PropertyCell cell) {
 		String colorGroup = cell.getColorGroup();
 		int propertyNumber = getPropertyNumberForColor(colorGroup);
-		colorGroups.put(colorGroup, new Integer(propertyNumber + 1));
+		colorGroups.put(colorGroup, propertyNumber + 1);
         cells.add(cell);
 	}
 
--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/PropertyCell.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/PropertyCell.java
@@ -7,22 +7,43 @@ public class PropertyCell extends Cell {
 	private int rent;
 	private int sellPrice;
 
+	/**
+	 * Returns the color group to which this property belongs.
+	 * @return the color group of the property
+	 */
 	public String getColorGroup() {
 		return colorGroup;
 	}
 
+	/**
+	 * Returns the price required to build a house on this property.
+	 * @return the house building price
+	 */
 	public int getHousePrice() {
 		return housePrice;
 	}
 
+	/**
+	 * Returns the current number of houses built on this property.
+	 * @return the number of houses on the property
+	 */
 	public int getNumHouses() {
 		return numHouses;
 	}
     
+    @Override
+    /**
+     * Returns the selling price of this property.
+     * @return the sell price of the property
+     */
     public int getPrice() {
 		return sellPrice;
 	}
 
+	/**
+	 * Calculates and returns the rent amount to be charged based on ownership and houses.
+	 * @return the rent amount to charge
+	 */
 	public int getRent() {
 		int rentToCharge = rent;
 		String [] monopolies = theOwner.getMonopolies();
@@ -33,6 +54,12 @@ public class PropertyCell extends Cell {
 		return rentToCharge;
 	}
 
+	/**
+	 * Calculates adjusted rent by doubling it if the property color group is part of the owner's monopolies.
+	 * @param rentToCharge the base rent amount before monopoly adjustment
+	 * @param monopolies an array of color groups owned as monopolies by the player
+	 * @return the adjusted rent after monopoly check
+	 */
 	private int calculateMonopoliesRent(int rentToCharge, String[] monopolies) {
 		for(int i = 0; i < monopolies.length; i++) {
 			if(monopolies[i].equals(colorGroup)) {
@@ -42,6 +69,9 @@ public class PropertyCell extends Cell {
 		return rentToCharge;
 	}
 
+	/**
+	 * Executes the property's action when a player lands on it, charging rent if owned by another player.
+	 */
 	public void playAction() {
 		Player currentPlayer = null;
 		if(!isAvailable()) {
@@ -52,22 +82,42 @@ public class PropertyCell extends Cell {
 		}
 	}
 
+	/**
+	 * Sets the color group for this property.
+	 * @param colorGroup the color group to assign to the property
+	 */
 	public void setColorGroup(String colorGroup) {
 		this.colorGroup = colorGroup;
 	}
 
+	/**
+	 * Sets the price to build a house on this property.
+	 * @param housePrice the house building price to set
+	 */
 	public void setHousePrice(int housePrice) {
 		this.housePrice = housePrice;
 	}
 
+	/**
+	 * Sets the number of houses currently built on this property.
+	 * @param numHouses the number of houses to set
+	 */
 	public void setNumHouses(int numHouses) {
 		this.numHouses = numHouses;
 	}
 
+	/**
+	 * Sets the selling price for this property.
+	 * @param sellPrice the sell price to assign
+	 */
 	public void setPrice(int sellPrice) {
 		this.sellPrice = sellPrice;
 	}
 
+	/**
+	 * Sets the base rent amount for this property without monopoly adjustments.
+	 * @param rent the rent amount to set
+	 */
 	public void setRent(int rent) {
 		this.rent = rent;
 	}
